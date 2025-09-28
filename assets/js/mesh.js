2b7ca92f073f906eaed1b404529be60314bc57f3
Arlo Watts <arlowatts1@gmail.com>
2025-09-27 23:11:12 -0700

    Implement interpolation

--- assets/js/mesh.js
+++ assets/js/mesh.js
@@ -1,16 +1,16 @@
 import { faceLookup, vertexLookup } from './lookups.js';
 
-// return a mesh approximating a shape
-export function mesh(shape, resolution) {
+// return a mesh approximating a solid
+export function mesh(solid, resolution) {
 
-    // get the rectangular bounding volume of the shape
-    const bounds = shape.bounds();
+    // get the rectangular bounding volume of the solid
+    const bound = solid.bound();
 
     // compute the number of grid points needed to cover the volume
     const size = [
-        Math.ceil((bounds[1][0] - bounds[0][0]) / resolution) + 2,
-        Math.ceil((bounds[1][1] - bounds[0][1]) / resolution) + 2,
-        Math.ceil((bounds[1][2] - bounds[0][2]) / resolution) + 2,
+        Math.ceil((bound[1][0] - bound[0][0]) / resolution) + 2,
+        Math.ceil((bound[1][1] - bound[0][1]) / resolution) + 2,
+        Math.ceil((bound[1][2] - bound[0][2]) / resolution) + 2,
     ];
 
     // allocate an array for the signed distance grid
@@ -19,18 +19,18 @@ export function mesh(shape, resolution) {
     // initialize an array for the resulting faces
     const faces = [];
 
-    // evaluate the signed distance to the shape at each point on the grid
+    // evaluate the signed distance to the solid at each point on the grid
     for (let k = 0; k < size[2]; k++) {
         for (let j = 0; j < size[1]; j++) {
             for (let i = 0; i < size[0]; i++) {
 
                 // compute the position of the current grid point
-                const x = bounds[0][0] + (i - 0.5) * resolution;
-                const y = bounds[0][1] + (j - 0.5) * resolution;
-                const z = bounds[0][2] + (k - 0.5) * resolution;
+                const x = bound[0][0] + (i - 0.5) * resolution;
+                const y = bound[0][1] + (j - 0.5) * resolution;
+                const z = bound[0][2] + (k - 0.5) * resolution;
 
-                // store the signed distance to the shape
-                signedDistances[i + size[0] * j + size[0] * size[1] * k] = shape.signedDistance(x, y, z);
+                // store the signed distance to the solid
+                signedDistances[i + size[0] * j + size[0] * size[1] * k] = solid.signedDistance(x, y, z);
             }
         }
     }
@@ -59,9 +59,9 @@ export function mesh(shape, resolution) {
                 );
 
                 // compute the position of the current grid point
-                const x = bounds[0][0] + (i - 0.5) * resolution;
-                const y = bounds[0][1] + (j - 0.5) * resolution;
-                const z = bounds[0][2] + (k - 0.5) * resolution;
+                const x = bound[0][0] + (i - 0.5) * resolution;
+                const y = bound[0][1] + (j - 0.5) * resolution;
+                const z = bound[0][2] + (k - 0.5) * resolution;
 
                 // access the face information from the lookup table
                 for (let face of faceLookup[faceIndex]) {
@@ -73,14 +73,20 @@ export function mesh(shape, resolution) {
 
                     // convert the vertices to global coordinates
                     face = face.map(
-                        (vertex) => [
-                            x + vertex[0] * 0.5 * resolution,
-                            y + vertex[1] * 0.5 * resolution,
-                            z + vertex[2] * 0.5 * resolution,
-                        ]
+                        (vertex) => {
+                            const vertexPosition = [
+                                x + resolution * vertex[2][0],
+                                y + resolution * vertex[2][1],
+                                z + resolution * vertex[2][2],
+                            ];
+
+                            vertexPosition[vertex[0]] += resolution * corners[vertex[1][0]] / (corners[vertex[1][0]] - corners[vertex[1][1]]);
+
+                            return vertexPosition;
+                        }
                     );
 
-                    // triangulate the face
+                    // triangulate the face as a triangle fan
                     for (let n = 2; n < face.length; n++) {
 
                         // append the triangle to the mesh
